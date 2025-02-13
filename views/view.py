6a5987ce33f7f0ac18f5396c02a548d1852e6321
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-02-13 20:07:22 -0300

    add transfer balance functionality between accounts

--- views/view.py
+++ views/view.py
@@ -59,10 +59,43 @@ def remover_conta(id):
         session.commit()
 
         return conta
+    
 
-conta = Conta(nome = "Sabrina", banco = Bancos.SANTANDER, tipo = "Conta Corrente", saldo = 3000.0, usuario_id = 2, status = StatusConta.ATIVA)  
+def transferir_saldo(conta_origem_id, conta_destino_id, valor):
+    with Session(engine) as session:
+        statement = select(Conta).where(Conta.id == conta_origem_id)
+        results = session.execute(statement).scalars().all()
 
-criar_conta(conta)
+        if not results:
+            print("Conta de origem não encontrada")
+            return
+
+        conta_origem = results[0]
+        if conta_origem.saldo < valor:
+            print("Saldo insuficiente")
+            return
+
+        statement = select(Conta).where(Conta.id == conta_destino_id)
+        results = session.execute(statement).scalars().all()
+
+        if not results:
+            print("Conta de destino não encontrada")
+            return
+
+        conta_destino = results[0]
+        conta_origem.saldo -= valor
+        conta_destino.saldo += valor
+        session.add(conta_origem)
+        session.add(conta_destino)
+        session.commit()
+
+        return conta_origem, conta_destino   
+
+conta = Conta(nome = "Sabrina", banco = Bancos.SANTANDER, tipo = "Conta Corrente", saldo = 3000.0, usuario_id = 2, status = StatusConta.ATIVA) 
+conta2 = Conta(nome = "hidan", banco = Bancos.INTER, tipo = "Conta Corrente", saldo = 3000.0, usuario_id = 2, status = StatusConta.ATIVA) 
+conta3 = Conta(nome = "konan", banco = Bancos.INTER, tipo = "Conta Corrente", saldo = 3000.0, usuario_id = 3, status = StatusConta.ATIVA)
+criar_conta(conta3)
 print(listar_contas())
 print(desativar_conta(2))
+print(transferir_saldo(2, 3, 1000))
 
